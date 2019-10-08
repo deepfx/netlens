6e07ed4dde9c1b55f2747fa599f3343c1e6b77d2
Cesar Fuentes <cesar.fuentes@ricardo.ch>
2019-10-08 16:26:07 +0200

    latest cnn example

--- mnist_example.py
+++ mnist_example.py
@@ -1,5 +1,6 @@
 import gzip
 import pickle
+from math import ceil
 from pathlib import Path
 from typing import Tuple, Callable, Optional
 
@@ -7,7 +8,7 @@ import numpy as np
 import requests
 import torch
 import torch.nn.functional as F
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from torch import Tensor, nn, optim
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import TensorDataset, DataLoader
@@ -32,26 +33,55 @@ def download_data() -> Tuple[Tensor, ...]:
         return tuple(map(torch.tensor, (x_train, y_train, x_valid, y_valid)))
 
 
-def show_data_example(x_train: Tensor) -> None:
-    pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
-    pyplot.show()
-    print(x_train.shape)
+def show_data_example(x: Tensor) -> None:
+    plt.imshow(x.reshape((28, 28)), cmap="gray")
+    plt.show()
 
 
-class Mnist_CNN(nn.Module):
+class Trace(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+        self.stored = None
 
-    def forward(self, xb):
-        xb = xb.view(-1, 1, 28, 28)
-        xb = F.relu(self.conv1(xb))
-        xb = F.relu(self.conv2(xb))
-        xb = F.relu(self.conv3(xb))
-        xb = F.avg_pool2d(xb, 4)
-        return xb.view(-1, xb.size(1))
+    def forward(self, input):
+        self.stored = input
+        return input
+
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        return self.func(x)
+
+
+def preprocess(x):
+    return x.view(-1, 1, 28, 28)
+
+
+class MyNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            Lambda(preprocess),
+            nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            Trace(),
+            nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            Trace(),
+            nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            Trace(),
+            nn.AvgPool2d(4),
+            Lambda(lambda x: x.view(x.size(0), -1)),
+        )
+        self.traces = tuple(l for l in self.layers if isinstance(l, Trace))
+
+    def forward(self, input):
+        return self.layers(input)
 
 
 def get_data(train_ds: TensorDataset, valid_ds: TensorDataset, bs: int):
@@ -88,24 +118,49 @@ def fit(epochs: int, model: nn.Module, loss_func: Callable, opt: Optimizer, trai
         print(epoch, val_loss)
 
 
-def main():
+def train(bs=64, lr=0.1, epochs=4):
     x_train, y_train, x_valid, y_valid = download_data()
-    show_data_example(x_train)
+    show_data_example(x_train[0])
 
     train_ds = TensorDataset(x_train, y_train)
     valid_ds = TensorDataset(x_valid, y_valid)
 
-    bs = 64
     train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
 
-    lr = 0.1
-    model = Mnist_CNN()
+    model = MyNet()
     opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     loss_func = F.cross_entropy
 
-    epochs = 2
     fit(epochs, model, loss_func, opt, train_dl, valid_dl)
+    return model, opt, train_dl, valid_dl
+
+
+def show_activations(model: nn.Module, x: Tensor, normalize_layers=True, ncols=10, figsize=(20, 10)):
+    ypred = model(x)
+
+    nrows = 1 + sum(ceil(t.stored.shape[1] / ncols) for t in model.traces)
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(nrows, ncols, 1)
+    ax.imshow(x.reshape((28, 28)), cmap="gray")
+
+    pos = ncols + 1
+    for l, layer in enumerate(model.traces):
+        kernels = layer.stored.detach().numpy()
+        num_channels = kernels.shape[1]
+        vmin, vmax = (kernels.min(), kernels.max()) if normalize_layers else (None, None)
+        for c in range(num_channels):
+            ax = fig.add_subplot(nrows, ncols, pos + c)
+            ax.imshow(kernels[0, c, :, :], vmin=vmin, vmax=vmax)
+            ax.set_title('L%02d C%02d' % (l, c))
+            ax.axis('off')
+        pos += ceil(num_channels / ncols) * ncols
+
+    plt.show()
+
+    print(ypred)
+    return ypred
 
 
 if __name__ == '__main__':
-    main()
+    train()
