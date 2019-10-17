cd8d4430ec04e47f464ed21b700123f426833ab7
strasser.ms@gmail.com <strasser.ms@gmail.com>
2019-10-17 14:35:31 +0000

    moved bc git sucks

--- neural_transfer/modules.py
+++ neural_transfer/modules.py
@@ -4,12 +4,14 @@ from typing import Optional, List, Tuple, Iterable
 import torch
 import torch.nn.functional as F
 from torch import nn
-
+from pydash.arrays import find_last_index
+from torch import optim
 from .utils import gram_matrix, clean_layer
 
 
 class Normalization(nn.Module):
     def __init__(self, mean, std):
+        
         super(Normalization, self).__init__()
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
@@ -69,8 +71,8 @@ class LayeredModule(nn.Module):
     # TODO: implement similar static methods for other archs
 
     def forward(self, x):
-        for layer in (self.preprocessor, self.layers, self.postprocessor):
-            if layer:
+        for layer in ([self.preprocessor] + list(self.layers) + [self.postprocessor]):
+            if layer is not None:
                 x = layer(x)
         return x
 
@@ -102,25 +104,36 @@ class LayeredModule(nn.Module):
     def insert_after(self, insertion_key: Tuple[type, int], layer: nn.Module):
         module, nth = insertion_key
         idx = self.find_indices(module)[nth]
-        self.layers.insert(idx, layer)
+        self.layers.insert(idx+1, layer)
 
 
 class StyleTransferModule(LayeredModule):
     content_target: torch.Tensor
     style_target: torch.Tensor
+<<<<<<< HEAD
+
+    def __init__(self, arch: LayeredModule,
+=======
             
     def __init__(self, base: LayeredModule,
+>>>>>>> dbba94965756d59ae93c7d6bbea7eff0387a5b9e
                  content_target=None,
                  content_layer_keys=None,
                  style_target=None,
                  style_layer_keys=None):
-        super(StyleTransferModule, self).__init__(base.layers, base.preprocessor, base.postprocessor)
+        arch = copy.deepcopy(arch)
+        super(StyleTransferModule, self).__init__(arch.layers, arch.preprocessor, arch.postprocessor)
         self.content_target = content_target
         self.style_target = style_target
-        if content_target and content_layer_keys:
+        if content_target is not None and content_layer_keys is not None:
             self._insert_loss_layers(ContentLoss, content_target, content_layer_keys)
-        if style_target and style_layer_keys:
+        if style_target is not None and style_layer_keys is not None:
             self._insert_loss_layers(StyleLoss, style_target, style_layer_keys)
+        
+        
+        last = find_last_index(self.layers, lambda layer: isinstance(layer, StyleLoss) or isinstance(layer, ContentLoss))
+        self.layers = self.layers[:last+1]
+                             
 
     def _insert_loss_layers(self, layer_constructor, target, insertion_keys):
         target_at_layers = self.evaluate_at_layers(target, insertion_keys)
@@ -132,28 +145,32 @@ class StyleTransferModule(LayeredModule):
             # insert it after layer at key
             self.insert_after(key, loss_layer)
 
-    def run_style_transfer(self, input_img, optimizer_class, num_steps=300, style_weight=1000000, content_weight=1, verbose=False):
+    def run_style_transfer(self, input_img, optimizer_class=optim.LBFGS, num_steps=300, style_weight=1000000, content_weight=1, verbose=True):
         optimizer = optimizer_class([input_img.requires_grad_()])
+        
+        print("optimizing>....")
+        
+        
+        run = [0]
+        while run[0] <= num_steps:
+            def closure():
+                # correct the values of updated input image
+                input_img.data.clamp_(0, 1)
 
-        def closure(n):
-            # correct the values of updated input image
-            input_img.data.clamp_(0, 1)
+                optimizer.zero_grad()
+                self.forward(input_img)
+                style_score = style_weight * sum(sl.loss for sl in self.get_modules(StyleLoss))
+                content_score = content_weight * sum(cl.loss for cl in self.get_modules(ContentLoss))
+                loss = style_score + content_score
+                loss.backward()
+                
+                run[0] += 1
+                if verbose and run[0] % 50 == 0:
+                    print("run {}:".format(run))
+                    print('Style Loss : {:4f} Content Loss: {:4f}\n'.format(style_score.item(), content_score.item()))
+                return style_score + content_score
 
-            optimizer.zero_grad()
-            self.forward(input_img)
-            style_score = style_weight * sum(sl.loss for sl in self.get_modules(StyleLoss))
-            content_score = content_weight * sum(cl.loss for cl in self.get_modules(ContentLoss))
-            loss = style_score + content_score
-            loss.backward()
-
-            if verbose and n % 50 == 0:
-                print("run {}:".format(n))
-                print('Style Loss : {:4f} Content Loss: {:4f}\n'.format(style_score.item(), content_score.item()))
-
-            return style_score + content_score
-
-        for step in range(num_steps):
-            optimizer.step(lambda: closure(step))
+            optimizer.step(closure)
 
         # a last correction...
         input_img.data.clamp_(0, 1)
