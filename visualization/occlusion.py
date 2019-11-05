01e68e480e3249719a6e3478f907432959fc2ffa
Cesar Fuentes <cesar.at.fuentes@gmail.com>
2019-11-05 01:41:41 +0100

    fixing occlusion

--- visualization/occlusion.py
+++ visualization/occlusion.py
@@ -1,4 +1,5 @@
 # custom function to conduct occlusion experim
+import math
 import seaborn as sns
 import skimage
 import torch.nn.functional as F
@@ -23,15 +24,24 @@ def show_occlusion_tiles(image, occ_size=50, occ_stride=50):
 
 def occlusion(model, image, label, occ_size=50, occ_stride=50, occ_pixel=0):
     # get the width and height of the image
-    width, height = image.shape[-2:]
+    height, width = image.shape[-2:]
 
     # setting the output image width and height
-    output_height = (height - occ_size) // occ_stride
-    output_width = (width - occ_size) // occ_stride
+    # output_height = (height - occ_size) // occ_stride + 1
+    # output_width = (width - occ_size) // occ_stride + 1
+    # output_height = math.ceil(height / occ_stride)
+    # output_width = math.ceil(width / occ_stride)
+    def get_output_dim(dim):
+        output_dim = math.ceil(dim / occ_stride)
+        return output_dim - (1 if (output_dim - 1) * occ_stride == dim else 0)
+
+    output_height = get_output_dim(height)
+    output_width = get_output_dim(width)
     # create a white image of sizes we defined
 
     heatmap = torch.zeros((output_height, output_width))
     classmap = torch.zeros((output_height, output_width))
+    scaled_map = torch.zeros((height, width))
 
     # iterate all the pixels in each column
     for h in range(0, output_height):
@@ -40,17 +50,20 @@ def occlusion(model, image, label, occ_size=50, occ_stride=50, occ_pixel=0):
             w_start = w * occ_stride
             h_end = min(height, h_start + occ_size)
             w_end = min(width, w_start + occ_size)
+            if h_start == h_end or w_start == w_end: continue
 
             input_image = image.clone().detach()
 
             # replacing all the pixel information in the image with occ_pixel(grey) in the specified location
-            input_image[:, :, w_start:w_end, h_start:h_end] = occ_pixel
+            input_image[:, :, h_start:h_end, w_start:w_end] = occ_pixel
 
             # run inference on modified image
             # setting the heatmap location to probability value
-            heatmap[h, w], classmap[h, w] = prob_for_label(model, input_image, label)
+            prob, classmap[h, w] = prob_for_label(model, input_image, label)
+            heatmap[h, w] = prob
+            scaled_map[h_start:h_end, w_start:w_end] = prob  # * torch.ones((h_end - h_start, w_end - w_start))
 
-    return heatmap, classmap
+    return heatmap, classmap, scaled_map
 
 
 def prob_for_label(model, image, label):
@@ -62,17 +75,20 @@ def prob_for_label(model, image, label):
 
 
 def main():
-    original_img, name, target_class = get_example_data(3, img_path='../old_visual/input_images/')
+    original_img, name, target_class = get_example_data(1, img_path='../old_visual/input_images/')
 
     prep_img = preprocess_image(original_img)
 
     m_orig = models.vgg19_bn(pretrained=True)
     m_orig.eval()
 
-    hm, cm = occlusion(m_orig, prep_img, target_class, occ_size=20, occ_stride=20, occ_pixel=0)
+    hm, cm, hm_scaled = occlusion(m_orig, prep_img, target_class, occ_size=50, occ_stride=50, occ_pixel=0)
     prob_no_occ = torch.max(hm)
 
-    hm_scaled = skimage.transform.resize(hm, prep_img.shape[-2:], order=0)
+    hmu = torch.unique(hm_scaled)
+    print(hm_scaled.size(), hmu, hmu.size())
+
+    # hm_scaled = skimage.transform.resize(hm, prep_img.shape[-2:], order=0)
 
     _, axes = plt.subplots(1, 3, figsize=(15, 8))
 
