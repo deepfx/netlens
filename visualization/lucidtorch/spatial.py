dad3b8ef57f493ea195a65bc05be75103ff10ce9
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-14 15:51:31 +0000

    more adaptation for style transfer
    Former-commit-id: 87908e7289126d5fb7c9fa97a572e1a34ba3b892 [formerly 16c6c2c2efd66278a4da05f50fd6f14a857c4775]
    Former-commit-id: 60a926149287fd91b116ae160a8c41d7dae80089
    Former-commit-id: 23d108a51086e05d82a2e637230cee8f15e7b8d6

--- visualization/lucidtorch/spatial.py
+++ visualization/lucidtorch/spatial.py
@@ -18,7 +18,7 @@ def _rfft2d_freqs(h, w):
 
 
 def fft_image(shape, sd=None, decay_power=1):
-    b, h, w, ch = shape
+    b, ch, h, w = shape
     imgs = []
     for _ in range(b):
         freqs = _rfft2d_freqs(h, w)
@@ -27,15 +27,16 @@ def fft_image(shape, sd=None, decay_power=1):
         # init_val = sd * np.random.randn(2, ch, fh, fw).astype("float32")
         # spectrum_var = tf.Variable(init_val)
         # spectrum = tf.complex(spectrum_var[0], spectrum_var[1])
-        spectrum = torch.randn((2, ch, fh, fw), requires_grad=True)
+        spectrum = torch.randn((ch, fh, fw, 2), requires_grad=True)
         spectrum_scale = 1.0 / np.maximum(freqs, 1.0 / max(h, w)) ** decay_power
         # Scale the spectrum by the square-root of the number of pixels
         # to get a unitary transformation. This allows to use similar
         # learning rates to pixel-wise optimisation.
-        spectrum_scale *= np.sqrt(w * h)
-        scaled_spectrum = spectrum * torch.from_numpy(spectrum_scale.astype('float32'))
+        spectrum_scale = torch.from_numpy(spectrum_scale.astype('float32'))
+        print(f'fft_image: spectrum.shape={spectrum.shape}, scale.shape={spectrum_scale.shape}')
+        scaled_spectrum = spectrum * spectrum_scale[None,:,:,None]
         # img = tf.spectral.irfft2d(scaled_spectrum)
-        img = torch.irfft(scaled_spectrum.permute((1, 2, 3, 0)), signal_ndim=2)
+        img = torch.irfft(scaled_spectrum, signal_ndim=2)
         # in case of odd input dimension we cut off the additional pixel
         # we get from irfft2d length computation
         img = img[:ch, :h, :w]
