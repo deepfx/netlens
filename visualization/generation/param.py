fc22d2d0aa94cd107daa49a3a738b6568f018ee6
Cesar Fuentes <cesar.at.fuentes@gmail.com>
2019-11-18 16:03:40 +0100

    another try
    Former-commit-id: 2095f04c21c583d31fbfb000522c08d62176d3be [formerly e9675345bd059d2fc411ffd385fa271d1f96f490] [formerly 5324f0438abe60b81af9a1e57de22b665c1a7d22] [formerly 70c09a3e53ed6aab83cd42a6832b68bdd4c301d8 [formerly 6702ccfedd9812c463b6f5fe80a02feeaef20d2b]]
    Former-commit-id: 001728a1f8431d7aba5feaae6ff36e023c0e1de2 [formerly eec50030a859f08b6e485c01a2e81aba353628f8]
    Former-commit-id: 24ffe31c7683f761353f4090612241ba19ad264b
    Former-commit-id: 5f139cc0a00ba323fb35713fd08b01cf17888f12

---
+++ visualization/generation/param.py
@@ -0,0 +1,60 @@
+import torch
+from torch.nn import Module, Parameter
+
+
+class RawParam(Module):
+    """
+    A raw 'parameterized image', that just wraps a normal tensor.
+    """
+
+    def __init__(self, input: torch.Tensor, cloned: bool = True):
+        super().__init__()
+        self.param = Parameter(input.clone().detach().requires_grad_() if cloned else input)
+
+    def forward(self):
+        return self.param
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}: {self.param.shape}'
+
+
+# class ImageParam(Module):
+#     """Class to create a parameterized image.
+#
+#     Parameters:
+#     size (int): size of image. Image will be square.
+#     fft (bool): parameterise the image in the Fourier domain.
+#     decorrelate (bool): decorrelate the colours of the image.
+#     sigmoid (bool): apply sigmoid after decorrelation to ensure
+#         values are in range(0,1)
+#     mean (list): means of the images used to train the network.
+#     std (list): standard deviations of the images used to train the network.
+#     kwargs: passed on to the image function fourier_image or random_im.
+#     """
+#
+#     def __init__(self, size, fft=True, decorrelate=True, sigmoid=True, **kwargs):
+#         super().__init__()
+#         self.fft = fft
+#         self.decorrelate = decorrelate
+#         self.size = size
+#         self.sigmoid = sigmoid
+#
+#         im_func = fourier_image if fft else random_im
+#         self.noise, self.get_image = im_func(size, **kwargs)
+#         self.noise = Parameter(self.noise)
+#
+#     def forward(self):
+#         im = self.get_image(self.noise)
+#
+#         if self.decorrelate:
+#             im = _linear_decorrelate_color(im)
+#
+#         if self.sigmoid:
+#             # if we sigmoid, we should also normalize
+#             im = torch.sigmoid(im)
+#             im = norm(im, input_range=(0, 1), unsqueeze=False, grad=False, mean=self.mean, std=self.std)
+#
+#         return im
+#
+#     def __repr__(self):
+#         return f"{self.__class__.__name__}: {self.size}px, fft={self.fft}, decorrelate={self.decorrelate}"
