a72ea243ceef64ee28152add2d28c511dc94fe38
Cesar Fuentes <cesar.at.fuentes@gmail.com>
2019-11-19 17:12:38 +0100

    cleaning code; adding vis tfms
    Former-commit-id: 5b0c53dd825d01950d60326c72f777bfbfc3393c [formerly 2af6970ff83dd87ee940b484269271ef8dedbdd1]
    Former-commit-id: 7e9a4c499e18f0e4cef0ca0412164c5487c22a9d
    Former-commit-id: 9d810c6f754b7a491e81a1cb310d4b53c2a3c387

--- visualization/modules.py
+++ visualization/modules.py
@@ -85,13 +85,13 @@ def get_nested_layers(model: nn.Module, dont_flatten: Collection[type] = None) -
 class Normalization(nn.Module):
     name: str = 'normalization'
 
-    def __init__(self, mean, std):
+    def __init__(self, mean: torch.Tensor, std: torch.Tensor):
         super(Normalization, self).__init__()
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = mean.clone().view(-1, 1, 1)
+        self.std = std.clone().view(-1, 1, 1)
 
     def forward(self, img):
         # normalize img
--- visualization/transforms.py
+++ visualization/transforms.py
@@ -1,8 +1,9 @@
 from typing import Tuple
 
 from PIL import Image
+from torchvision.transforms import RandomCrop, RandomAffine, ToPILImage, ToTensor, Compose
 
-__all__ = ['Thumbnail']
+__all__ = ['Thumbnail', 'Jitter', 'VIS_TFMS']
 
 
 class Thumbnail(object):
@@ -18,3 +19,42 @@ class Thumbnail(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(size={0}, resample={1})'.format(self.size, self.resample)
+
+
+class Jitter(RandomCrop):
+    """
+    Emulates the "jitter" transform from Tensorflow/Lucid, which is basically a random crop but where the decrease in size is provided, instead of the
+    final size.
+    """
+
+    def __init__(self, d: int, *args, **kwargs):
+        # we need to have a valid value of size until an actual image comes
+        super(Jitter, self).__init__((0, 0), *args, **kwargs)
+        self.d = d
+
+    def __call__(self, img: Image.Image):
+        w, h = img.size
+        self.size = (h - self.d, w - self.d)
+        return super.__call__(img)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(d={0}, padding={1})'.format(self.d, self.padding)
+
+
+"""
+standard_transforms = [
+    pad(12, mode='constant', constant_value=.5),
+    jitter(8),
+    random_scale([1 + (i-5)/50. for i in range(11)]),
+    random_rotate(list(range(-10, 11)) + 5*[0]),
+    jitter(4),
+  ]
+"""
+
+VIS_TFMS = Compose([
+    ToPILImage(),
+    Jitter(8, padding=12, fill=128, padding_mode='constant'),  # pad + jitter from lucid together here
+    RandomAffine(degrees=(-10, 10), scale=[1 + (i - 5) / 50. for i in range(11)]),  # random_scale + random_rotate from lucid together here
+    Jitter(4),
+    ToTensor()
+])
